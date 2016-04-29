f7fdb4f458e792efcf3cbcd5bab1be1f6b9173b2
niminjie <niminjie@baidu.com>
2016-04-29 18:22:07 +0800

    update

--- erji/erji/spiders/erji_spider.py
+++ erji/erji/spiders/erji_spider.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import scrapy
 from erji.items import TopicItem
 from scrapy.http.request import Request
@@ -46,7 +48,7 @@ class ErjiSpider(scrapy.Spider):
     name = "erji"
     allowed_domains = ["www.erji.net"]
     start_urls = [
-        "http://www.erji.net/read.php?tid=1911530"
+        "http://www.erji.net/read.php?tid=1910542"
     ]
 
     def parseTopics(self, response):
@@ -60,8 +62,27 @@ class ErjiSpider(scrapy.Spider):
         return result
 
     def parse(self, response):
-        detailBox = response.xpath('//*[@id="main"]/form/div[@class="t t2"]/table');
+        baseUrl = response.url
+        detailBox = response.xpath('//*[@id="main"]/form/div[@class="t t2"]/table')
+
+        # 总共有多少页
+        lastPageUrl = response.xpath('//div[@class="pages"][1]/a[last()]/@href').extract()[0]
+        totalPage = lastPageUrl.split('=')[-1]
+
+        urls = [baseUrl + '2&fpage=0&toread=&page=' + i for i in range(totalPage)]
+
+        print urls
 
         for box in detailBox:
+            # 楼主/层主
             name = box.xpath('./tr[1]/th[1]/b/text()').extract_first().encode('UTF8');
             postedTime = box.xpath('./tr[2]/th/div/text()').extract_first().encode('UTF8').strip()[8:-2].replace('Posted: ', '');
+            floor = box.xpath('./tr[2]/th/div/span[2]/a/text()').extract_first().encode('UTF8').strip();
+
+            # 回帖内容
+            content = box.xpath('./tr[1]/th[2]/div[@class="tpc_content"]').extract_first();
+
+
+            # print name, postedTime, floor
+
+            # yield Request(url=baseUrl + , callback=self.parseDetail)
